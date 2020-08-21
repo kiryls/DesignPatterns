810ff9b083994bac4e851222918dd33d3ae4b43c
Kiryl Shvachka <kiryl92@gmail.com>
2020-08-21 21:26:35 +0200

    migliorato il client

--- Observer/WeatherStation.java
+++ Observer/WeatherStation.java
@@ -1,15 +1,25 @@
 package Observer;
 
-import Observer.observers.CurrentConditionsDisplay;
+import Observer.observers.*;
 
 public class WeatherStation {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
 
         CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
 
+        System.out.println();
         weatherData.setMeasurements(80, 65, 30.4f);
+        System.out.println();
         weatherData.setMeasurements(82, 70, 29.2f);
+        System.out.println();
         weatherData.setMeasurements(78, 90, 29.2f);
+        System.out.println();
+        weatherData.removeObserver(forecastDisplay);
+        weatherData.setMeasurements(62, 90, 28.1f);
+        System.out.println();
     }
 }
