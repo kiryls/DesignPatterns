9f55c6157ea6526a31e44f3577e67070399654df
Kiryl Shvachka <kiryl92@gmail.com>
2020-08-21 21:15:39 +0200

    added getters to weather data object

--- Observer/WeatherData.java
+++ Observer/WeatherData.java
@@ -39,4 +39,16 @@ public class WeatherData implements Subject{
         this.pressure = pressure;
         measurementsChanged();
     }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }    
 }
