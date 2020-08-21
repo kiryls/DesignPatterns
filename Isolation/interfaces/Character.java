3d381de0d2807100ac3411e541f5327ac319e176
Kiryl Shvachka <kiryl92@gmail.com>
2020-08-21 18:01:04 +0200

    updated Character

--- Isolation/interfaces/Character.java
+++ Isolation/interfaces/Character.java
@@ -2,12 +2,28 @@ package Isolation.interfaces;
 
 public abstract class Character {
     protected String name;
-    protected int baseHP = 10;
-    protected int baseSP = 10;
-    protected int baseMP = 10;
+    protected int baseHP = 10; // base health
+    protected int baseSP = 10; // base stamina
+    protected int baseMP = 10; // base mana
 
     protected AtkMode atkM;
 
     public abstract void performAtk();
     public abstract void status();
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHP() {
+        return baseHP;
+    }
+
+    public int getSP() {
+        return baseSP;
+    }
+
+    public int getMP() {
+        return baseMP;
+    }
 }
