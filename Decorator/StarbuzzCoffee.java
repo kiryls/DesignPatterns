32fd48bfac4dfe80abcbf43179e75809937d578b
Kiryl Shvachka <kiryl92@gmail.com>
2020-08-22 10:57:02 +0200

    better client

--- Decorator/StarbuzzCoffee.java
+++ Decorator/StarbuzzCoffee.java
@@ -2,26 +2,76 @@ package Decorator;
 
 import Decorator.beverages.*;
 import Decorator.decorators.*;
+import java.util.Scanner;
 
 public class StarbuzzCoffee {
     public static void main(String[] args) {
-        Beverage b0 = new Espresso();
-        System.out.println();
-        System.out.println(b0.getDescription() + ": total $" + b0.cost());
+        Scanner input = new Scanner(System.in);
+        boolean finished = false;
+        Beverage beverage = null;
 
-        Beverage b1 = new DarkRoast();
-        b1 = new Mocha(b1);
-        b1 = new Mocha(b1);
-        b1 = new Whip(b1);
-        System.out.println();
-        System.out.println(b1.getDescription() + ": total $" + b1.cost());
+        System.out.println("\n=== Welcome to Starbuzz Coffee ===\nChoose your coffee:");
+        System.out.println("[1] House Blend: $.89 | [2] Dark Roast $.99 | [3] Decaf $1.05 | [4] Espresso $1.99");
+        int choice = input.nextInt();
 
-        Beverage b2 = new HouseBlend();
-        b2 = new Soy(b2);
-        b2 = new Mocha(b2);
-        b2 = new Whip(b2);
+        switch (choice) {
+            case 1:
+                beverage = new HouseBlend();
+                break;
+            
+            case 2:
+                beverage = new DarkRoast();
+                break;
+
+            case 3:
+                beverage = new Decaf();
+                break;
+
+            case 4:
+                beverage = new Espresso();
+                break;
+        
+            default:
+                finished = true;
+                input.close();
+                System.out.println("Finished");
+                break;
+        }
+
+        System.out.println("\nChoose your condiments:");
+        while(!finished) {
+            System.out.println("[1] Steamed milk: $.10 | [2] Mocha $.20 | [3] Soy $.15 | [4] Whip $.10 | [0] Proceed to order");
+            choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    beverage = new SteamedMilk(beverage);
+                    break;
+
+                case 2:
+                    beverage = new Mocha(beverage);
+                    break;
+
+                case 3:
+                    beverage = new Soy(beverage);
+                    break;
+
+                case 4:
+                    beverage = new Whip(beverage);
+                    break;
+
+                case 0:
+                    finished = true;
+                    System.out.println("Finished");
+                    break;
+
+                default:
+                    System.out.println("(err: wrong choice)");
+                    break;
+            }
+        }
+
         System.out.println();
-        System.out.println(b2.getDescription() + ": total $" + b2.cost());
+        System.out.println(beverage.getDescription() + ": total $" + beverage.cost());
         System.out.println();
     }
 }
