18c2cab40d9bf9ca3a10efc9dfae1fca21cb67c9
Kiryl Shvachka <kiryl92@gmail.com>
2020-08-22 18:36:01 +0200

    tipi di pizze concrete

---
+++ Factory/products/CheesePizza.java
@@ -0,0 +1,17 @@
+package Factory.products;
+
+import Factory.Pizza;
+
+public class CheesePizza extends Pizza {
+    public void prepare() {
+    }
+    
+    public void bake() {
+    }
+
+    public void cut() {
+    }
+
+    public void box() {
+    }
+}
---
+++ Factory/products/VeggiePizza.java
@@ -0,0 +1,17 @@
+package Factory.products;
+
+import Factory.Pizza;
+
+public class VeggiePizza extends Pizza {
+    public void prepare() {
+    }
+
+    public void bake() {
+    }
+
+    public void cut() {
+    }
+
+    public void box() {
+    }
+}
