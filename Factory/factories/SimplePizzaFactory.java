10a28b2ae9fe1a0c000837e665b52a3567438584
Kiryl Shvachka <kiryl92@gmail.com>
2020-08-22 18:41:10 +0200

    simplefactory fix

--- Factory/factories/SimplePizzaFactory.java
+++ Factory/factories/SimplePizzaFactory.java
@@ -1,8 +1,24 @@
 package Factory.factories;
 
+import Factory.products.*;
+import Factory.Pizza;
+
 public class SimplePizzaFactory {
-    public void createPizza(String type) {
+    public Pizza createPizza(String type) {
+        Pizza pizza = null;
 
         if(type.equals("cheese"))
+            pizza = new CheesePizza();
+        
+        else if(type.equals("pepperoni"))
+            pizza = new PepperoniPizza();
+
+        else if(type.equals("clam"))
+            pizza = new ClamPizza();
+
+        else if(type.equals("veggie"))
+            pizza = new VeggiePizza();
+
+        return pizza;
     }
 }
