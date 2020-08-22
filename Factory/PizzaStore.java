d287704b208574f8c49fc7a091c4597b67e2588a
Kiryl Shvachka <kiryl92@gmail.com>
2020-08-22 18:35:44 +0200

    client distributore pizze

---
+++ Factory/PizzaStore.java
@@ -0,0 +1,19 @@
+package Factory;
+
+public abstract class PizzaStore {
+
+    public Pizza orderPizza(String type) {
+        Pizza pizza;
+
+        pizza = createPizza(type);
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+    }
+
+    public abstract Pizza createPizza(String type);
+}
