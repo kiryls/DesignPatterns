6133f59e482e6d7870fd0358d815cfe20560918c
Kiryl Shvachka <kiryl92@gmail.com>
2020-08-31 19:46:43 +0200

    updated pizza factories to ingredient factories

---
+++ Factory/factories/NYPizzaIngredientFactory.java
@@ -0,0 +1,34 @@
+package Factory.factories;
+
+import Factory.ingredients.*;
+import Factory.ingredients.interfaces.*;
+import Factory.interfaces.*;
+
+public class NYPizzaIngredientFactory implements PizzaIngredientFactory {
+
+    public Dough createDough() {
+        return new ThinCrustDough();
+    }
+
+    public Sauce createSauce() {
+        return new MarinaraSauce();
+    }
+
+    public Cheese createCheese() {
+        return new ReggianoCheese();
+    }
+
+    public Veggies[] createVeggies() {
+        Veggies veggies[] = { new Garlic(), new Onion(), new Mushroom(), new RedPepper() };
+        return veggies;
+    }
+
+    public Pepperoni createPepperoni() {
+        return new SlicedPepperoni();
+    }
+
+    public Clams createClam() {
+        return new FreshClams();
+    }
+    
+}
